fdb38b26393828481ed13a0c010bfabcd838e8a8
jannes.t13@gmail.com <jannes.t13@gmail.com>
2022-06-18 20:21:33 +0200

    Add database upload method

--- data/fetch_memes.py
+++ data/fetch_memes.py
@@ -1,5 +1,9 @@
 import praw
 from config import CLIENT_ID, CLIENT_SECRET, USER_AGENT, USERNAME
+from database.database import Database
+import asyncio
+
+db = Database()
 
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
@@ -10,7 +14,7 @@ reddit = praw.Reddit(
 
 subreddit = reddit.subreddit("memes")
 
-def get_meme():
+def get_meme() -> str:
     """GET MEME FROM REDDIT"""
 
     subreddit = reddit.subreddit("memes")
@@ -18,12 +22,13 @@ def get_meme():
 
     return meme.url
 
-def upload_meme_to_db(meme:str):
+async def upload_meme_to_db(meme:str) -> None:
     """UPLOAD MEME TO DB"""
+    await db.init_memes_db(meme)
     return
 
 for _ in range(15):
     meme = get_meme()
-    upload_meme_to_db(meme)
+    asyncio.run(upload_meme_to_db(meme))
 
 print("Done!")
